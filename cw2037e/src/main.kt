b4caddeae0bc654c9c5b4b6ec128f3e2c5a4f872
José Pedro Dias <jose.dias@sky.uk>
2024-11-23 10:21:55 +0000

    setting up the example

--- cw2037e/src/main.kt
+++ cw2037e/src/main.kt
@@ -1,8 +1,15 @@
 private fun readStr() = readln()
 fun readInt() = readStr().toInt()
+fun readAnswerInt(): Int {
+    val a = readInt()
+    val tmp = readStr()
+    //dbg("tmp: <$tmp>")
+    return a
+}
 
 private fun write(s: String) {
     println(s)
+    println()
     System.out.flush()
 }
 
@@ -11,7 +18,7 @@ fun writeQuery(l: Int, r: Int) {
 }
 
 fun writeImp() {
-    write("IMPOSSIBLE")
+    write("! IMPOSSIBLE")
 }
 
 fun writeAnswer(answer: BooleanArray) {
@@ -19,31 +26,93 @@ fun writeAnswer(answer: BooleanArray) {
     write("! ${answer2.joinToString("")}")
 }
 
+fun dbg(s: String) {
+    //System.err.println(s)
+}
+
 fun computeCandidates(n: Int): List<BooleanArray> {
     // TODO
     return emptyList()
 }
 
+data class RunData(
+    val n: Int,
+    val answers: Map<Pair<Int, Int>, Int> = kotlin.collections.mutableMapOf()
+)
 //fun subAnswer()
 
 fun main() {
-    val answers = mutableMapOf<Pair<Int, Int>, Int>()
-    var l = 0
-    var r = 0
-
-    l = 1
-    r = 4
-    writeQuery(l, r)
-    answers[Pair(l, r)] = readInt()
-
-    l = 2
-    r = 3
-    writeQuery(l, r)
-    answers[Pair(l, r)] = readInt()
-
-    //writeImp()
-
-    val ans = BooleanArray(4)
-    ans[1] = true
-    writeAnswer(ans)
+    try {
+        val testCases = readInt()
+        dbg("testCases: $testCases")
+        for (tc in 1..testCases) {
+            val n = readAnswerInt()
+            val d = RunData(n)
+            dbg("tc #: $tc, n: $n")
+            val answers = mutableMapOf<Pair<Int, Int>, Int>()
+            var i = 0
+            while (true) {
+                if (tc == 1) {
+                    if (i == 0) {
+                        val l = 1
+                        val r = 5
+                        writeQuery(l, r)
+                        val answer = readAnswerInt()
+                        dbg("got $answer")
+                        answers[Pair(l, r)] = answer
+                    } else if (i == 1) {
+                        val l = 2
+                        val r = 4
+                        writeQuery(l, r)
+                        val answer = readAnswerInt()
+                        dbg("got $answer")
+                        answers[Pair(l, r)] = answer
+                    } else if (i == 2) {
+                        val l = 4
+                        val r = 5
+                        writeQuery(l, r)
+                        val answer = readAnswerInt()
+                        dbg("got $answer")
+                        answers[Pair(l, r)] = answer
+                    } else if (i == 3) {
+                        val l = 3
+                        val r = 5
+                        writeQuery(l, r)
+                        val answer = readAnswerInt()
+                        dbg("got $answer")
+                        answers[Pair(l, r)] = answer
+                    } else if (i == 4) {
+                        val guess = BooleanArray(n)
+                        guess[1] = true
+                        guess[4] = true
+                        writeAnswer(guess)
+                        dbg("answers: $answers")
+                        break
+                    } else if (i == 10) {
+                        writeImp()
+                        dbg("answers: $answers")
+                        break
+                    }
+                } else if (tc == 2) {
+                    if (i == 0) {
+                        val l = 1
+                        val r = 2
+                        writeQuery(l, r)
+                        val answer = readAnswerInt()
+                        dbg("got $answer")
+                        answers[Pair(l, r)] = answer
+                    } else if (i == 1) {
+                        writeImp()
+                        dbg("answers: $answers")
+                        break
+                    }
+                }
+                ++i
+            }
+        }
+        dbg("all done")
+    } catch (err: Throwable) {
+        dbg("oops")
+        dbg("$err")
+    }
 }
